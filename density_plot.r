93e07ea21e248db8543fc56c5c1f8adb376f1638
Fan Wang <fan1@uchicago.edu>
2016-10-30 22:31:19 -0500

    Update density_plot.r

--- density_plot.r
+++ density_plot.r
@@ -1,8 +1,14 @@
+
 cor.table=read.csv("R and p value in Tumor(420) and Normal(36).csv")
 
+###density plot
 plot (density(cor.table$R.in.tumor), col="red", lwd=2) 
 lines(density(cor.table$R.in.normal), col="green", lwd=2) 
 
 
 plot (density(cor.table$R.square.in.tumor), col="red", lwd=2) 
 lines(density(cor.table$R.square.in.normal), col="green", lwd=2) 
+
+###scatter plot
+plot(cor.table$R.square.in.tumor, cor.table$R.square.in.normal,  pch=19,cex=.5) 
+abline(lm(cor.table$R.square.in.tumor~cor.table$R.square.in.normal))
