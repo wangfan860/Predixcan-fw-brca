053802c5a8207b855c1b1e53ab63847b5b96e125
Fan Wang <fan1@uchicago.edu>
2016-12-04 11:38:55 -0600

    Update Reading multiple csv files from a folder into a single dataframe.r

--- Reading multiple csv files from a folder into a single dataframe.r
+++ Reading multiple csv files from a folder into a single dataframe.r
@@ -1,3 +1,8 @@
 file_names <- dir() #where you have your files
-
 your_data_frame <- do.call(cbind,lapply(file_names,read.csv))
+write.csv(file_names,"44tissueNames.csv")
+write.csv(your_data_frame,"tumor-pearson-result.csv")
+
+tissue=read.csv("44tissueNames.csv")
+tissue3=tissue[rep(seq_len(nrow(tissue)), each=3),]  ##triplicate all the value
+write.csv(tissue3,"3Xall tissue type.csv")
